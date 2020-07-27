09dc104f0f798bff2b467a1e262a1badba363a5a
Swarnya Srivastava <49378967+Swarnya246@users.noreply.github.com>
2020-07-26 17:26:51 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,9 +7,9 @@ import datetime
 
 
 # edit these three variables
-user = 'realdonaldtrump'
+user = 'elonmusk'
 start = datetime.datetime(2010, 1, 1)  # year, month, day
-end = datetime.datetime(2016, 12, 7)  # year, month, day
+end = datetime.datetime(2020, 7, 25)  # year, month, day
 
 # only edit these if you're having problems
 delay = 1  # time to wait on each page load before reading the page
